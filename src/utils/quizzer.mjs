46d5914f140c307b778adb3bf420919f42b40726
Máirtín MacMíoltóg <mairtinmacmioltog@gmail.com>
2022-11-02 20:34:49 -0400

    Adds All Ireland data

--- src/utils/quizzer.mjs
+++ src/utils/quizzer.mjs
@@ -1,48 +1,54 @@
-import fifaWC from '../data/fifaWC.json' assert {type: 'json'};
-import superBowl from '../data/superBowl.json' assert {type: 'json'};
-import { difference } from './setOperations.mjs';
+import fifaWC from "../data/fifaWC.json" assert { type: "json" };
+import superBowl from "../data/superBowl.json" assert { type: "json" };
+import allIrlFoot from "../data/allIrlFoot.json" assert { type: "json" };
+import allIrlHurl from "../data/allIrlHurl.json" assert { type: "json" };
+import { difference } from "./setOperations.mjs";
 
 function occurences(tournament, state) {
-    const frequencyMap = new Map();
-    tournament.forEach((t) => {
-        state.forEach((s) => {
-            if (frequencyMap.has(t[s])) {
-                frequencyMap.set(t[s], frequencyMap.get(t[s]) + 1);
-            }
-            else {
-                frequencyMap.set(t[s], 1);
-            }
-        });
-
+  const frequencyMap = new Map();
+  tournament.forEach((t) => {
+    state.forEach((s) => {
+      if (frequencyMap.has(t[s])) {
+        frequencyMap.set(t[s], frequencyMap.get(t[s]) + 1);
+      } else {
+        frequencyMap.set(t[s], 1);
+      }
     });
-    return new Map([...frequencyMap.entries()].sort((a, b) => b[1] - a[1]));
-};
+  });
+  return new Map([...frequencyMap.entries()].sort((a, b) => b[1] - a[1]));
+}
 
 function winnerHosts(tournament) {
-    const frequencyMap = new Map();
-    tournament.forEach((t) => {
-        if (t.winner === t.country) {
-            if (frequencyMap.has(t.winner)) {
-                frequencyMap.set(t.winner, frequencyMap.get(t.winner) + 1);
-            }
-            else {
-                frequencyMap.set(t.winner, 1);
-            }
-        }
-    });
-    return new Map([...frequencyMap.entries()].sort((a, b) => b[1] - a[1]));
+  const frequencyMap = new Map();
+  tournament.forEach((t) => {
+    if (t.winner === t.country) {
+      if (frequencyMap.has(t.winner)) {
+        frequencyMap.set(t.winner, frequencyMap.get(t.winner) + 1);
+      } else {
+        frequencyMap.set(t.winner, 1);
+      }
+    }
+  });
+  return new Map([...frequencyMap.entries()].sort((a, b) => b[1] - a[1]));
 }
 
 function noWinFinalists(tournament) {
-    const winnerSet = new Set(Array.from(occurences(tournament, ["winner"]).keys()));
-    const loserSet = new Set(Array.from(occurences(tournament, ["loser"]).keys()));
-    return difference(loserSet, winnerSet);
+  const winnerSet = new Set(
+    Array.from(occurences(tournament, ["winner"]).keys())
+  );
+  const loserSet = new Set(
+    Array.from(occurences(tournament, ["loser"]).keys())
+  );
+  return difference(loserSet, winnerSet);
 }
 
 function attendance(tournament) {
-    return tournament.sort((a, b) => a.attendance - b.attendance);
+  return tournament.sort((a, b) => a.attendance - b.attendance);
 }
 
+console.log("------------------------------");
+console.log("--------- WORLD CUP ----------");
+console.log("------------------------------");
 console.log("winner", occurences(fifaWC, ["winner"]));
 console.log("loser", occurences(fifaWC, ["loser"]));
 console.log("finalist", occurences(fifaWC, ["winner", "loser"]));
@@ -52,7 +58,9 @@ console.log("winning hosts", winnerHosts(fifaWC));
 console.log("never won", noWinFinalists(fifaWC));
 console.log("attendance", attendance(fifaWC));
 
-
+console.log("------------------------------");
+console.log("--------- SUPER BOWL ---------");
+console.log("------------------------------");
 console.log("winner", occurences(superBowl, ["winner"]));
 console.log("loser", occurences(superBowl, ["loser"]));
 console.log("finalist", occurences(superBowl, ["winner", "loser"]));
@@ -60,3 +68,20 @@ console.log("host", occurences(superBowl, ["state"]));
 console.log("city", occurences(superBowl, ["city"]));
 console.log("never won", noWinFinalists(superBowl));
 console.log("attendance", attendance(superBowl));
+
+
+console.log("------------------------------");
+console.log("-------- ALL IRL FOOT --------");
+console.log("------------------------------");
+console.log("winner", occurences(allIrlFoot, ["winner"]));
+console.log("loser", occurences(allIrlFoot, ["loser"]));
+console.log("finalist", occurences(allIrlFoot, ["winner", "loser"]));
+console.log("never won", noWinFinalists(allIrlFoot));
+
+console.log("------------------------------");
+console.log("-------- ALL IRL HURL --------");
+console.log("------------------------------");
+console.log("winner", occurences(allIrlHurl, ["winner"]));
+console.log("loser", occurences(allIrlHurl, ["loser"]));
+console.log("finalist", occurences(allIrlHurl, ["winner", "loser"]));
+console.log("never won", noWinFinalists(allIrlHurl));
